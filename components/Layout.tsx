22d05f0dd87d97de536f02f803ac4f9136eacd7f
Pedro Fagundes <pedrohefagundes@gmail.com>
2022-03-18 07:55:47 +0000

    page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { FC } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
@@ -5,6 +6,9 @@ import Navbar from "./Navbar";
 const Layout: FC = ({ children }) => {
   return (
     <div className="layout w100vw">
+        <Head>
+          <title>Pedro Fagundes</title>
+        </Head>
         <Navbar />
         { children }
         <Footer />
