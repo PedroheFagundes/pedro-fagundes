4692f6b51d82ac6a1fa8d60c5962106bb40b7e42
Pedro Fagundes <pedrohefagundes@gmail.com>
2022-04-30 14:06:38 +0100

    zoom-disabled

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"></link>
       </Head>
       <Layout>
