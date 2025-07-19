f8d1d144b40d32c2ba6af4242b39e7a86778a413
Pedro Fagundes <81723158+PedroheFagundes@users.noreply.github.com>
2025-07-19 09:37:44 -0300

    Update _documents.js

--- pages/_documents.js
+++ pages/_documents.js
@@ -5,6 +5,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5474022631580710"
+     crossorigin="anonymous"></script>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
